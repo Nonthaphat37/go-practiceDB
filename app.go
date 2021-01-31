83033504e07f8ac630c82be6c9d38d627f1b2464
nonthaphat <nonthaphat37@gmail.com>
2021-01-31 23:55:29 +0700

    fixed method post

--- app.go
+++ app.go
@@ -22,6 +22,7 @@ type App struct{
 
 func (a *App) initializeRoutes(){
 	a.Router.HandleFunc("/user/{id:[0-9]+}", a.getUser).Methods("GET");
+	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
 }
 
 func (a *App) Initialize(user, password, dbName string){
@@ -41,7 +42,7 @@ func (a *App) Initialize(user, password, dbName string){
 	})
 
 	pong, err := a.Redis.Ping().Result()
-	fmt.Println(pong, err)
+	fmt.Println("Test ping redis", pong, err);
 
 	a.initializeRoutes();
 }
@@ -52,8 +53,6 @@ func respondWithError(w http.ResponseWriter, code int, message string){
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
 	response, _ := json.Marshal(payload)
-	
-	fmt.Println(payload, string(response));
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
@@ -64,19 +63,18 @@ func (a *App) getUser(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r);
 	id, err := strconv.Atoi(vars["id"]);
 	if err != nil {
-		fmt.Println(err);
 		respondWithError(w, http.StatusBadRequest, "Invalid User ID");
 		return;
 	}
-
 	defer r.Body.Close()
 
 	u := user{ID: id};
 
+	fmt.Println("Get Mathod");
 	if val, err := u.getUserRedis(a.Redis); err != redis.Nil {
 		data := user{};
-		json.Unmarshal([]byte(val), &data)
-		fmt.Println(data)
+		json.Unmarshal([]byte(val), &data);
+		fmt.Println("Get user from redis");
 		respondWithJSON(w, http.StatusOK, data);
 	} else{
 		if err := u.getUserDB(a.DB); err != nil {
@@ -86,11 +84,52 @@ func (a *App) getUser(w http.ResponseWriter, r *http.Request){
 				respondWithError(w, http.StatusInternalServerError, err.Error());
 			}
 		} else{
+			fmt.Println("Get user from DB");
 			respondWithJSON(w, http.StatusOK, u);
+
+			fmt.Println("Set user to redis");
+			err := u.setUserRedis(a.Redis);
+			if err != nil {
+				fmt.Println("Error set from redis", err);
+			}
 		}
 	}
 }
 
+func (a *App) createUser(w http.ResponseWriter, r *http.Request){
+	var u user;
+	decoder := json.NewDecoder(r.Body);
+	if err := decoder.Decode(&u); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload");
+		return;
+	}
+	defer r.Body.Close();
+
+	tmp := u;
+	fmt.Println("Post Mathod");
+	if err := u.getUserDB(a.DB); err != nil {
+		if err == sql.ErrNoRows {
+			if err := u.createUser(a.DB); err != nil {
+				respondWithError(w, http.StatusInternalServerError, err.Error());
+			} else{
+				respondWithJSON(w, http.StatusCreated, u);
+			}
+		} else{
+			respondWithError(w, http.StatusInternalServerError, err.Error());
+		}
+	} else{
+		u = tmp;
+		if err := u.updateUser(a.DB); err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error());
+		} else{
+			respondWithJSON(w, http.StatusOK, u);
+			err := u.delUserRedis(a.Redis);
+			if err != nil {
+				fmt.Println("Error delete from redis", err);
+			}
+		}
+	}
+}
 
 func (a *App) Run(addr string){
 	log.Fatal(http.ListenAndServe(addr, a.Router));
--- model.go
+++ model.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	// "fmt"
+	"encoding/json"
 	"strconv"
 	"database/sql"
 	"github.com/go-redis/redis"
@@ -14,6 +16,9 @@ type user struct {
 
 const (
 	getUserCommand = "SELECT firstname, lastname FROM users WHERE id=$1";
+	createUserCommand = "INSERT INTO users(id, firstname, lastname) VALUES($1, $2, $3) RETURNING id";
+	updateUserCommand = "UPDATE users SET firstname=$1, lastname=$2 WHERE id=$3";
+	CACHE_TTL = 0;
 )
 
 func (u *user) getUserRedis(Redis *redis.Client) (string, error){
@@ -21,7 +26,28 @@ func (u *user) getUserRedis(Redis *redis.Client) (string, error){
 	return val, err
 }
 
+func (u *user) delUserRedis(Redis *redis.Client) error{
+	err := Redis.Del(strconv.Itoa(u.ID)).Err();
+	return err;
+}
+
+func (u *user) setUserRedis(Redis *redis.Client) error{
+	json, _ := json.Marshal(u)
+	err := Redis.Set(strconv.Itoa(u.ID), json, CACHE_TTL).Err()
+	return err;
+}
+
 func (u *user) getUserDB(db *sql.DB) error{
 	err := db.QueryRow(getUserCommand, u.ID).Scan(&u.Firstname, &u.Lastname);
 	return err;
+}
+
+func (u *user) createUser(db *sql.DB) error{
+	err := db.QueryRow(createUserCommand, u.ID, u.Firstname, u.Lastname).Scan(&u.ID)
+	return err;
+}
+
+func (u *user) updateUser(db *sql.DB) error{
+	_, err := db.Exec(updateUserCommand, u.Firstname, u.Lastname, u.ID)
+	return err
 }
